a44708d6de9597c47c7f87f4acda4212c66a4d4c
Juan Hernandez <juanhernandez9731@gmail.com>
2025-04-14 14:03:28 -0700

    subtotal functionality

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from './StateProvider';
 import "./Subtotal.css";
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
     const [{basket},dispatch] = useStateValue();
@@ -21,7 +22,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0} // Part of the homework
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
--- src/reducer.js
+++ src/reducer.js
@@ -4,6 +4,10 @@ export const initialState = {
   };
 
 
+// Selector
+export const getBasketTotal = (basket) => 
+  basket?.reduce((amount, item) => item.price + amount, 0);
+
 
 
   const reducer = (state, action) => {
