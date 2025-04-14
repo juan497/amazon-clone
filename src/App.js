aae1207c6ffd87828349fdebd98a5d3b56c0958e
Juan Hernandez <juanhernandez9731@gmail.com>
2025-04-14 10:21:23 -0700

    made checkout have products from basket.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Balls from "./Balls";
 import Home from "./Home";
+import Checkout from "./Checkout";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Home text="home page" />} />
             <Route path="/login" element={<Balls text="login" />} />
-            <Route path="/checkout" element={<Balls text="checkout" />} />
+            <Route path="/checkout" element={<Checkout text="checkout" />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Checkout.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./Checkout.css"
+import Header from './Header'
+import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
+
+function Checkout(props) {
+    const [{ basket, user }, dispatch] = useStateValue();
+  return (
+    
+    <div className='checkout'>
+
+        <Header/>
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
+        <h1>{props.text}</h1>
+        <div>
+          <h3>Hello, {user?.email}</h3>
+          <h2 className="checkout__title">Your shopping Basket</h2>
+
+          {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+
+        </div>
+    </div>
+  )
+}
+
+export default Checkout
