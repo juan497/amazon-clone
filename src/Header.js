8e354ec6fe457a6e67cbcf73cde4d04ace293185
Juan Hernandez <juanhernandez9731@gmail.com>
2025-04-13 17:29:26 -0700

    quick showcase of data layer using basket.

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css"
 import { Link } from 'react-router-dom'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { useStateValue } from './StateProvider';
+
 function Header() {
+  const [{basket}] = useStateValue();
+
   return (
     <nav className="header">
         <Link to="/">
@@ -45,7 +49,7 @@ function Header() {
         <Link to="/checkout"className='header__link' >
           <div className='header__optionBasket'>
             <ShoppingBasketIcon/>
-            <span className="header__optionLineTwo header__basketCount">0</span>
+            <span className="header__optionLineTwo header__basketCount">{basket.length}</span>
           </div>
         </Link>
 
